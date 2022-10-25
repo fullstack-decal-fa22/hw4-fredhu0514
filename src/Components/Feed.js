3ab11391ad84069bfa58630d73e108c5e843eeb6
Fred Hu <huyuqi0514@126.com>
2022-10-24 23:00:56 -0700

    cool

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import Menu from './Menu';
 import Block from './Block';
@@ -6,15 +6,18 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [postNames, setPosts] = useState([]);
 
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    function handleClick(val) {
+        setPosts(oldArr => [val, ...oldArr]);
+    }
+
+    const posts = postNames.map((objName) => <Block color={objName} />);
 
     return (
         <div>
-            <Menu></Menu>
+            <Menu handler={handleClick} />
 
-            {/* Below is where all of your Blocks should render! */}
             {posts}
         </div>
     );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import Color from './Color';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+            <Color color={'red'} handleClick={props.handler} />
+            <Color color={'salmon'} handleClick={props.handler} />
+            <Color color={'pink'} handleClick={props.handler} />
+            <Color color={'blue'} handleClick={props.handler} />
+            <Color color={'green'} handleClick={props.handler} />
+            <Color color={'yellow'} handleClick={props.handler} />
+            <Color color={'orange'} handleClick={props.handler} />
+            <Color color={'white'} handleClick={props.handler} />
+            <Color color={'gray'} handleClick={props.handler} />
+            <Color color={'black'} handleClick={props.handler} />
       </div>
     );
 }
